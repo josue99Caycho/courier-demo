7c88a5f2a60fef4a36acf3bcf0e0d8ba1663018f
jcaychob <jjbardalez@stefanini.com>
2023-04-17 18:07:21 -0500

    se agrego el menu

--- demo-courier/src/app/app.component.ts
+++ demo-courier/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'demo-courier';
+export class AppComponent implements OnInit {
+ 
+  menu: object [] = [];  
+
+  ngOnInit(): void {
+    this.menu = [
+      {label: 'Home'},
+      {label: 'Courier'},
+      {label: 'Exportaciones'},
+      {label: 'Softpad'}
+  ];
+  }
 }
