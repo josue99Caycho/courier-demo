5abf9348a8145831dfb627faccfd1be38cc588b5
jcaychob <jjbardalez@stefanini.com>
2023-04-17 16:50:07 -0500

    config primeng

--- demo-courier/src/app/app.module.ts
+++ demo-courier/src/app/app.module.ts
@@ -1,6 +1,69 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { ButtonModule } from 'primeng/button';
+import { AccordionModule } from 'primeng/accordion';
+import { AnimateModule } from 'primeng/animate';
+import { BadgeModule } from 'primeng/badge';
+import { BreadcrumbModule } from 'primeng/breadcrumb';
+import { CalendarModule } from 'primeng/calendar';
+import { CardModule } from 'primeng/card';
+import { CheckboxModule } from 'primeng/checkbox';
+import { DialogModule } from 'primeng/dialog';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputMaskModule } from 'primeng/inputmask';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { MegaMenuModule } from 'primeng/megamenu';
+import { ListboxModule } from 'primeng/listbox';
+import { DropdownModule } from 'primeng/dropdown';
+import { DynamicDialogModule } from 'primeng/dynamicdialog';
+import { DividerModule } from 'primeng/divider';
+import { PasswordModule } from 'primeng/password';
+import { TabViewModule } from 'primeng/tabview';
+import { MenubarModule } from 'primeng/menubar';
+import { TabMenuModule } from 'primeng/tabmenu';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { ToastModule } from 'primeng/toast';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { PaginatorModule } from 'primeng/paginator';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { SkeletonModule } from 'primeng/skeleton';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { TableModule } from 'primeng/table';
+
+const COMPONENTS = [
+  AnimateModule,
+  AccordionModule,
+  BadgeModule,
+  ButtonModule,
+  BreadcrumbModule,
+  CalendarModule,
+  CardModule,
+  CheckboxModule,
+  DialogModule,
+  InputTextModule,
+  InputMaskModule,
+  InputNumberModule,
+  MegaMenuModule,
+  ListboxModule,
+  DropdownModule,
+  DynamicDialogModule,
+  DividerModule,
+  PasswordModule,
+  TabViewModule,
+  MenubarModule,
+  TabMenuModule,
+  InputTextareaModule,
+  ToastModule,
+  ConfirmDialogModule,
+  PaginatorModule,
+  RadioButtonModule,
+  SkeletonModule,
+  InputSwitchModule,
+  TableModule
+];
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +73,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    COMPONENTS
   ],
   providers: [],
   bootstrap: [AppComponent]
